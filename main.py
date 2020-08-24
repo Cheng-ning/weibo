2301fc241f9771a84781380e3c3311216147ed74
洆宁 <351200797@qq.com>
2020-08-24 20:56:33 +0800

    项目初始化

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from libs.orm import db
+
+app = Flask(__name__)
+app.secret_key = r'zxicojnv90u3nlcnxvp39-vl39(*l31j'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/weibo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+manager = Manager(app)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+@app.route('/')
+def home():
+    """首页"""
+    return 'hello world'
+
+
+if __name__ == '__main__':
+    manager.run()
